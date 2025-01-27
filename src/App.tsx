d5b2869c317439996ec3cd5f04bbc63c54c94dca
alesbesdria <melissa@meirsman.fr>
2025-01-27 16:59:45 +0100

    second try?

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,30 @@ import "./App.css";
 import EmployeeCard from "./components/EmployeeCard";
 import { useState } from "react";
 
+const [employee, setFirstEmployee] = useState(null);
 
-// const fetchRandomEmployee = async () => {
-//   const response = await fetch("https://randomuser.me/api?nat=fr&inc=name,email,picture");
-//   const data = await response.json();
-//   return data.results[0];
-// };
-
-
-
-const sampleEmployee = {
-  name: {
-    first: "https://randomuser.me/api/?inc=name",
-    last: "https://randomuser.me/api/?inc=name",
-  },
-  email: "https://randomuser.me/api/?inc=email",
-  picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/66.jpg",
-  },
+const sampleFirstEmployee = () => {
+  // Send the request
+  fetch("https://randomuser.me/api?nat=en")
+    .then((response) => response.json())
+    .then((data) => {
+      setFirstEmployee(data.results[0]);
+    });
 };
 
+// const sampleEmployee = {
+//   name: {
+//     first: "https://randomuser.me/api/?gender=female,?firstname=angela",
+//     last: "https://randomuser.me/api/?gender=female,?lastname=dawson",
+//   },
+//   email: "https://randomuser.me/api/?email=angeladawson@gmail.com",
+//   picture: {
+//     medium: "https://randomuser.me/api/portraits/med/men/66.jpg",
+//   },
+// };
+
 function App() {
-  const [employee, setEmployee] = useState(sampleEmployee);
+  const [employee, setEmployee] = useState(sampleFirstEmployee);
 
   const getEmployee = () => {
     fetch("https://randomuser.me/api?nat=fr")
@@ -34,9 +36,11 @@ function App() {
   };
 
   return (
-    <div className='App'>
+    <div className="App">
       <EmployeeCard employee={employee} />
-      <button type="button" onClick={getEmployee}>Get employee</button>
+      <button type="button" onClick={getEmployee}>
+        Get employee
+      </button>
     </div>
   );
 }
